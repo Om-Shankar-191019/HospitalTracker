264df0b4b9899c32e8de20146bda1b12320e8e49
om shankar <om8003golu@gmail.com>
2023-02-10 15:16:33 +0530

    hospital card done

--- src/components/Material/HospitalPiece.js
+++ src/components/Material/HospitalPiece.js
@@ -1,13 +1,83 @@
 import React from 'react'
 import './HospitalPiece.css'
-const HospitalPiece = ({name,keyId,expandedId,onClick}) => {
+const HospitalPiece = ({name,keyId,expandedId,onClick,latitude,longitude,tags}) => {
+
+  const clinicDialog = [
+
+    "Get the care you deserve, right in your neighborhood.",
+    "Say goodbye to long wait times and hello to personalized care.",
+    "Small clinic, big on quality.",
+    "Where convenience meets affordability.",
+    "Healthcare tailored to your needs, just a short drive away.",
+    "A Better Way to Get the Care You Need.",
+    "Your One-Stop Shop for Comprehensive Healthcare Services.",
+    "Affordable, Accessible Healthcare for Your Whole Family.",
+    "Experience the Benefits of Personalized Care.",
+    "Your Health is Our Priority: The Advantages of Choosing a Small Clinic."
+    ];
+
+    const getRandom = (first,last) => {
+      let r = Math.random()*(last+1-first);
+      return(Math.floor(r)+first)
+    }
+
+    const writeQuotes = () =>{
+      let index = getRandom(0,clinicDialog.length);
+      return clinicDialog[index];
+    }
+
+ 
   return (
     <div className={`hospitalPiece-container ${expandedId === keyId ? "expanded" : ""}`} onClick={onClick}>
-        <img src='https://cdn-icons-png.flaticon.com/128/3580/3580415.png' alt="" />
-        <p>{name}</p>
+        
+        {expandedId !== keyId ? 
+          <>
+            <img src='https://cdn-icons-png.flaticon.com/128/3580/3580415.png' alt="" />
+            <p>{name}</p>
+          </>
+          :
+          <div className='hospital-card-container'>
+            <h3 style={{marginBottom:'12px'}}>{name}</h3>
+            <div className='flex-center'  >
+              <p style={{color:'green'}}>{!tags && writeQuotes()}</p>
+            </div>
+            <p className="color-blue" style={{marginBottom:'10px',color:'darkblue'}}>{tags && tags["addr:full"]}</p>
+            <div className='flex-center' >
+              <p>{tags && tags["addr:street"]}</p>
+              <p>{tags && tags["addr:city"]}</p>
+            </div>
+            <div className='flex-center' >
+              <p>{tags && tags["addr:district"]}</p>
+              <p>{tags && tags["addr:state"]}</p>
+            </div>
+            <div className='flex-center' >
+              <p>{tags && tags["addr:postcode"]}</p>
+            </div>
+            <div className='flex-center' >
+              <p>{latitude}</p>
+              <p>{longitude}</p>
+            </div>
+            <div style={{display:'flex',alignItems:'center',justifyContent:'space-around',marginTop:'18px'}}>
+              <button style={{marginRight:"10px"}}  className='add-blog-button'>Pin to map</button>
+              <button style={{marginRight:"10px"}}  className='add-blog-button'>Direction</button>
+            </div>
+            
+
+          </div>
+        }
+        {/* {tags && console.log("printing tags : ",tags["addr:district"])}
+        {tags && console.log("printing tags : ",tags["addr:state"])} */}
         
     </div>
   )
 }
 
 export default HospitalPiece
+
+
+
+
+
+
+
+
--- src/components/Material/Material.jsx
+++ src/components/Material/Material.jsx
@@ -7,14 +7,23 @@ import './Material.css'
 const Material = () => {
   const hospitalInfo = useSelector((state) => state.hospital.info);
   const [expandedId,setExpandedId] = useState(null);
-
+  // console.log(" info : ", hospitalInfo);
  
   return (
     <div className='material-container'>
       <BasicSelect />
       <div className='hospitalPiece-wrapper'>
         {hospitalInfo && hospitalInfo.length > 0 &&  hospitalInfo.map((item) => 
-            <HospitalPiece key={item.id} keyId={item.id} expandedId={expandedId} onClick={() => setExpandedId(expandedId === item.id ? null : item.id)} name={item.tags  ? item.tags.name ? item.tags.name : "clinic" : "clinic" } />
+            <HospitalPiece 
+              key={item.id} 
+              keyId={item.id} 
+              expandedId={expandedId} 
+              onClick={() => setExpandedId(expandedId === item.id ? null : item.id)} 
+              name={item.tags  ? item.tags.name ? item.tags.name : "clinic" : "clinic" }
+              latitude={item.lat}
+              longitude={item.lon} 
+              tags={item?.tags} 
+            />
           )
         }
       </div>
